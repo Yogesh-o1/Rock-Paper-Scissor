fa17099f4e2cd839d312db4f61d5150b650df963
Yogesh <141220341+Yogesh-o1@users.noreply.github.com>
2023-09-09 12:00:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,119 @@
+let ps=""
+let cs=""
+let ppts=0;
+let cpts=0;
+let n=0;
+let winner=""
+
+//console.table({psec:ps,csec:cs,Ppts:ppts,Cpts:cpts,result:winner});//
+function gamePlay(ps,cs){
+    switch (ps) {
+    case  "rock":
+        if(cs==="rock"){
+            winner="Tie"
+        }else if(cs==="paper"){
+            cpts++
+            winner="CPU wins"
+        }else{
+            ppts++
+            winner="PLAYER wins"
+        
+        }
+        break;
+    case "scissor":
+        if(cs==="scissor"){
+            winner="Tie"
+        }else if(cs==="rock"){
+        cpts++
+        winner="CPU wins"
+        }else{
+            ppts++
+            winner="PLAYER wins"
+        
+        }
+        break;
+    case "paper":
+        if(cs=="paper"){
+            winner="Tie"
+        }else if(cs==="scissor"){
+            cpts++
+            winner="CPU wins"
+        
+        }else{
+            ppts++
+            winner="PLAYER wins"
+        }
+        break;
+    }
+}
+    // DOM //
+    const rockbtn=document.getElementById("rbtn")
+    const paperbtn=document.getElementById("pbtn")
+    const scissorbtn=document.getElementById("sbtn")
+    const resetbtn=document.getElementById("resetbtn")
+    const playerPts=document.getElementById("pointsP")
+    const cpuPts=document.getElementById("pointsC")
+    const plSel=document.getElementById("pimage")
+    const cpuSel=document.getElementById("cpuimage")
+
+    rockbtn.addEventListener("click",() => fingerPlay("rock"))
+    paperbtn.addEventListener("click",() => fingerPlay("paper"))
+    scissorbtn.addEventListener("click",() => fingerPlay("scissor"))
+
+function cpuChoice(){
+    let x= Math.floor((Math.random()*3)+1)
+    switch(x){
+        case 1:
+            return "rock"
+        case 2:
+            return "paper" 
+        case 3:
+            return "scissor"
+    }
+}
+
+function fingerPlay(name){
+    ps=name
+    cs=cpuChoice()
+    gamePlay(ps,cs)
+    gameScore()
+    updateChoice(ps,cs)
+    }
+
+function gameScore(){
+    playerPts.textContent=`Pts:  ${ppts}`
+    cpuPts.textContent=`Pts:  ${cpts}`
+   
+
+}
+function updateChoice(ps,cs){
+    switch(ps){
+    case "rock":
+        plSel.textContent="🪨"
+        break;
+    case "paper":
+        plSel.textContent="📄"
+        break;
+    case "scissor":
+        plSel.textContent="✂️"
+        break;
+    }
+    switch(cs){
+    case"rock":
+        cpuSel.textContent="🪨"
+        break;
+    case "paper":
+        cpuSel.textContent="📄"
+        break;
+     case "scissor":
+        cpuSel.textContent="✂️"
+        break;
+    }
+}
+//✂️✌️⛰️📄🪨✊✋//
+/*function  scoreReset(){
+    ppts=0
+    cpts=0
+    ps=""
+    cs=""
+}*/
